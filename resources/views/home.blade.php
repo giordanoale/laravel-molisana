87a1cc2d97b38c4a8b66d9ee1d207886e559279d
giordanoale <a.giordano89@gmail.com>
2021-05-07 17:20:00 +0200

    Terminata sezione Main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,6 +27,45 @@
     <!-- corpo principale -->
     <main>
 
+        <!-- sezione "lunghe" -->
+        <div class="container">
+            <h2>Le Lunghe</h2>
+            <div class="cards">
+                @foreach($lunga as $pasta)
+                <div class="card">
+                    <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <!-- /sezione "lunghe" -->
+
+        <!-- /sezione "corte" -->
+        <div class="container">
+            <h2>Le Corte</h2>
+            <div class="cards">
+                @foreach($corta as $pasta)
+                <div class="card">
+                    <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <!-- /sezione "corte" -->
+
+        <!-- /sezione "cortissime" -->
+        <div class="container">
+            <h2>Le cortissime</h2>
+            <div class="cards">
+                @foreach($cortissima as $pasta)
+                <div class="card">
+                    <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <!-- /sezione "cortissime" -->
+
     </main>
     <!-- /corpo principale -->
 
