228a883cc87d7f92d25aa53899d8b8a3a55950c9
giordanoale <a.giordano89@gmail.com>
2021-05-07 16:28:38 +0200

    Terminato header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- /css -->
+
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <!-- google font -->
+
     <title>La Molisana</title>
 </head>
 
 <body>
-    
+
+    <!-- intestazione -->
+    @include('parts.header')
+    <!-- /intestazione -->
+
+    <!-- corpo principale -->
+    <main>
+
+    </main>
+    <!-- /corpo principale -->
+
+    <!-- pie di pagina -->
+    @include('parts.footer')
+    <!-- /pie di pagina -->
+
 </body>
 
 </html>
---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+
+    <div class="container">
+
+        <!-- logo -->
+        <div class="logo">
+            <img src="{{ asset('images/marchio-sito-test.png') }}" alt="">
+        </div>
+        <!-- /logo -->
+
+        <!-- barra di navigazione -->
+        <nav class="main-nav">
+            <ul>
+                <li><a class="active" href="#">Home</a></li>
+                <li><a href="#">Prodotti</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        </nav>
+        <!-- /barra di navigazione -->
+    </div>
+
+</header>
